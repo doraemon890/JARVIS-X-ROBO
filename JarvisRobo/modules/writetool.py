f81c7718f0d7d89c2c78100aa8ca1ea6635b5fb6
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-04-02 15:04:24 +0530

    Update writetool.py

--- JarvisRobo/modules/writetool.py
+++ JarvisRobo/modules/writetool.py
@@ -1,33 +1,11 @@
-"""MIT License
 
-Copyright (c) 2023-24 Noob-Mukesh
-
-          GITHUB: NOOB-MUKESH
-          TELEGRAM: @MR_SUKKUN
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE."""
 from pyrogram import filters
 from pyrogram.types import Message
 from JarvisRobo import BOT_NAME, BOT_USERNAME
-from JarvisRobo import pbot as mukesh
+from JarvisRobo import pbot as jarvis
 import requests
-@mukesh.on_message(filters.command("write"))
+
+@jarvis.on_message(filters.command("write"))
 async def handwrite(_, message: Message):
     if message.reply_to_message:
         text = message.reply_to_message.text
