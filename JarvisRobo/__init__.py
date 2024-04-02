23ff1a784757eaaba8987ade22ed41aa448c2e5e
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-04-02 18:33:27 +0530

    Update __init__.py

--- JarvisRobo/__init__.py
+++ JarvisRobo/__init__.py
@@ -45,9 +45,8 @@ if ENV:
     LOAD = os.environ.get("LOAD", "").split()
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     NO_LOAD = os.environ.get("NO_LOAD", "").split()
-    START_IMG = os.environ.get(
-        "START_IMG", ""
-    )
+    START_IMG = os.environ.get("START_IMG", "")
+    HELP_IMG = os.environ.get("HELP_IMG", "https://telegra.ph/file/61296490da95c55a1d5ee.jpg")
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Chatting_2024")
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
@@ -102,6 +101,7 @@ else:
     MONGO_DB_URI = Config.MONGO_DB_URI
     NO_LOAD = Config.NO_LOAD
     START_IMG = Config.START_IMG
+    HELP_IMG = Config.HELP_IMG
     STRICT_GBAN = Config.STRICT_GBAN
     SUPPORT_CHAT = Config.SUPPORT_CHAT
     TEMP_DOWNLOAD_DIRECTORY = Config.TEMP_DOWNLOAD_DIRECTORY
