e16b28f59676cbdf0eab67f5eed4f2911b897903
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-04-02 14:06:49 +0530

    Update encode_decode.py

--- JarvisRobo/modules/encode_decode.py
+++ JarvisRobo/modules/encode_decode.py
@@ -1,5 +1,5 @@
 import requests
-from .. import pbot as Mukesh,BOT_NAME,BOT_USERNAME
+from .. import pbot as Jarvis,BOT_NAME,BOT_USERNAME
 import time
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
