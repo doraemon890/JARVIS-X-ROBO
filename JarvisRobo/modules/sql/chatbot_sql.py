36de27d0e9724dd9b75be76f5ee18b7ded8d8c2e
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-04-02 12:58:51 +0530

    Update chatbot_sql.py

--- JarvisRobo/modules/sql/chatbot_sql.py
+++ JarvisRobo/modules/sql/chatbot_sql.py
@@ -5,38 +5,38 @@ from sqlalchemy import Column, String
 from JarvisRobo.modules.sql import BASE, SESSION
 
 
-class MukeshChats(BASE):
-    __tablename__ = "mukesh_chats"
+class JarvisChats(BASE):
+    __tablename__ = "jarvis_chats"
     chat_id = Column(String(14), primary_key=True)
 
     def __init__(self, chat_id):
         self.chat_id = chat_id
 
 
-MukeshChats.__table__.create(checkfirst=True)
+JarvisChats.__table__.create(checkfirst=True)
 INSERTION_LOCK = threading.RLock()
 
 
-def is_mukesh(chat_id):
+def is_jarvis(chat_id):
     try:
-        chat = SESSION.query(MukeshChats).get(str(chat_id))
+        chat = SESSION.query(JarvisChats).get(str(chat_id))
         return bool(chat)
     finally:
         SESSION.close()
 
 
-def set_mukesh(chat_id):
+def set_jarvis(chat_id):
     with INSERTION_LOCK:
-        mukeshchat = SESSION.query(MukeshChats).get(str(chat_id))
-        if not mukeshchat:
-            mukeshchat = MukeshChats(str(chat_id))
-        SESSION.add(mukeshchat)
+        jarvischat = SESSION.query(JarvisChats).get(str(chat_id))
+        if not jarvischat:
+            jarvischat = JarvisChats(str(chat_id))
+        SESSION.add(jarvischat)
         SESSION.commit()
 
 
-def rem_mukesh(chat_id):
+def rem_jarvis(chat_id):
     with INSERTION_LOCK:
-        mukeshchat = SESSION.query(MukeshChats).get(str(chat_id))
-        if mukeshchat:
-            SESSION.delete(mukeshchat)
+        jarvischat = SESSION.query(JarvisChats).get(str(chat_id))
+        if jarvischat:
+            SESSION.delete(jarvischat)
         SESSION.commit()
