b2a531762fc8bc0253c2011e1c7cb50bdcfed7b8
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-04-04 09:15:08 +0530

    Update tagall.py

--- JarvisRobo/modules/tagall.py
+++ JarvisRobo/modules/tagall.py
@@ -1,63 +1,51 @@
 import asyncio
-
 from telethon import events
 from telethon.errors import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
-
 from JarvisRobo import telethn as client
 
 spam_chats = []
 
-@client.on(events.NewMessage(pattern="^@tagall ?(.*)"))
-@client.on(events.NewMessage(pattern="^@all ?(.*)"))
-@client.on(events.NewMessage(pattern="^/tagall ?(.*)"))
-@client.on(events.NewMessage(pattern="^@mention ?(.*)"))
-async def mentionall(event):
+@client.on(events.NewMessage(pattern=r"^(@tagall|@all|/tagall|@mention) ?(.*)"))
+async def mention_all(event):
     chat_id = event.chat_id
+
     if event.is_private:
-        return await event.respond(
-            "__This command can be use in groups and channels!__"
-        )
+        return await event.respond("__This command can only be used in groups and channels!__")
 
     is_admin = False
     try:
-        partici_ = await client(GetParticipantRequest(event.chat_id, event.sender_id))
-    except UserNotParticipantError:
-        is_admin = False
-    else:
-        if isinstance(
-            partici_.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)
-        ):
+        participant = await client(GetParticipantRequest(event.chat_id, event.sender_id))
+        if isinstance(participant.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)):
             is_admin = True
+    except UserNotParticipantError:
+        pass
+
     if not is_admin:
         return await event.respond("__Only admins can mention all!__")
 
-    if event.pattern_match.group(1) and event.is_reply:
-        return await event.respond("__Give me one argument!__")
-    elif event.pattern_match.group(1):
-        mode = "text_on_cmd"
-        msg = event.pattern_match.group(1)
+    mode = "text_on_cmd"
+    msg = None
+
+    if event.pattern_match.group(2):
+        msg = event.pattern_match.group(2).strip()
     elif event.is_reply:
-        mode = "text_on_reply"
         msg = await event.get_reply_message()
-        if msg == None:
-            return await event.respond(
-                "__ɪ ᴄᴀɴ'ᴛ ᴍᴇɴᴛɪᴏɴ ᴍᴇᴍʙᴇʀs ғᴏʀ ᴏʟᴅᴇʀ ᴍᴇssᴀɢᴇs! (ᴍᴇssᴀɢᴇs ᴡʜɪᴄʜ ᴀʀᴇ sᴇɴᴛ ʙᴇғᴏʀᴇ ɪ'ᴍ ᴀᴅᴅᴇᴅ ᴛᴏ ɢʀᴏᴜᴘ__"
-            )
-    else:
-        return await event.respond(
-            "__Reply to a message or give me some text to mention others!__"
-        )
+        if msg is None:
+            return await event.respond("__I can't mention members for older messages!__")
+
+    if msg is None:
+        return await event.respond("__Reply to a message or provide some text to mention others!__")
 
     spam_chats.append(chat_id)
     usrnum = 0
     usrtxt = ""
-    async for usr in client.iter_participants(chat_id):
-        if not chat_id in spam_chats:
+    async for user in client.iter_participants(chat_id):
+        if chat_id not in spam_chats:
             break
         usrnum += 1
-        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}), "
+        usrtxt += f"[{user.first_name}](tg://user?id={user.id}), "
         if usrnum == 15:
             if mode == "text_on_cmd":
                 txt = f"{msg}\n{usrtxt}"
@@ -67,40 +55,31 @@ async def mentionall(event):
             await asyncio.sleep(3)
             usrnum = 0
             usrtxt = ""
-    try:
-        spam_chats.remove(chat_id)
-    except:
-        pass
 
+    spam_chats.remove(chat_id)
 
-@client.on(events.NewMessage(pattern="^/cancel$"))
+@client.on(events.NewMessage(pattern=r"^/cancel$"))
 async def cancel_spam(event):
-    if not event.chat_id in spam_chats:
-        return await event.respond("ᴛʜᴇʀᴇ ɪs ɴᴏ ᴘʀᴏᴄᴄᴇss ᴏɴ ɢᴏɪɴɢ..")
+    if event.chat_id not in spam_chats:
+        return await event.respond("There is no process going on.")
+
     is_admin = False
     try:
-        partici_ = await client(GetParticipantRequest(event.chat_id, event.sender_id))
-    except UserNotParticipantError:
-        is_admin = False
-    else:
-        if isinstance(
-            partici_.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)
-        ):
+        participant = await client(GetParticipantRequest(event.chat_id, event.sender_id))
+        if isinstance(participant.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)):
             is_admin = True
+    except UserNotParticipantError:
+        pass
+
     if not is_admin:
-        return await event.respond("__ᴏɴʟʏ ᴀᴅᴍɪɴs ᴄᴀɴ ᴇxᴇᴄᴜᴛᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ!__")
+        return await event.respond("__Only admins can execute this command!__")
 
-    else:
-        try:
-            spam_chats.remove(event.chat_id)
-        except:
-            pass
-        return await event.respond("sᴛᴏᴘᴘᴇᴅ ᴍᴇɴᴛɪᴏɴ.__")
-
+    spam_chats.remove(event.chat_id)
+    return await event.respond("Stopped mention.")
 
 __mod_name__ = "Tᴀɢᴀʟʟ"
 __help__ = """
 ──「  ᴏɴʟʏ ғᴏʀ ᴀᴅᴍɪɴs 」──
 
-❍ /tagall ᴏʀ @all '(ʀᴇᴘʟʏ ᴛᴏ ᴍᴇssᴀɢᴇ ᴏʀ ᴀᴅᴅ ᴀɴᴏᴛʜᴇʀ ᴍᴇssᴀɢᴇ) ᴛᴏ ᴍᴇɴᴛɪᴏɴ ᴀʟʟ ᴍᴇᴍʙᴇʀs ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ, ᴡɪᴛʜᴏᴜᴛ ᴇxᴄᴇᴘᴛɪᴏɴ.'
+❍ /tagall or @all '(ʀᴇᴘʟʏ ᴛᴏ ᴍᴇssᴀɢᴇ ᴏʀ ᴀᴅᴅ ᴀɴᴏᴛʜᴇʀ ᴍᴇssᴀɢᴇ) ᴛᴏ ᴍᴇɴᴛɪᴏɴ ᴀʟʟ ᴍᴇᴍʙᴇʀs ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ, ᴡɪᴛʜᴏᴜᴛ ᴇxᴄᴇᴘᴛɪᴏɴ.'
 """
