783bab536605d69ee0a5b14774024efa82c522bc
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-04-08 12:02:19 +0530

    Update admin.py

--- JarvisRobo/modules/admin.py
+++ JarvisRobo/modules/admin.py
@@ -883,27 +883,29 @@ def adminlist(update, context):
 
 
 __help__ = """
-*User Commands* :
-» /admins : list of admins in the chat.
-» /pinned : to get the current pinned message.
+» ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs ꜰᴏʀ Aᴅᴍɪɴs :
 
-*The Following Commands are Admins only : * 
-» /pin : silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users.
-» /unpin : unpins the currently pinned message.
-» /invitelink : gets invitelink.
-» /promote : promotes the user replied to.
-» /lowpromote : promotes the user replied to with half rights.
-» /fullpromote : promotes the user replied to with full rights.
-» /demote : demotes the user replied to.
-» /title <title here> : sets a custom title for an admin that the bot promoted.
-» /admincache : force refresh the admins list.
-» /del : deletes the message you replied to.
-» /purge : deletes all messages between this and the replied to message.
-» /purge <integer X> : deletes the replied message, and X messages following it if replied to a message.
-» /setgtile <text> : set group title.
-» /setgpic : reply to an image to set as group photo.
-» /setdesc : Set group description.
-» /setsticker : Set group sticker.
+User Commands:
+» /admins: list of admins in the chat
+» /pinned: to get the current pinned message.
+
+The Following Commands are Admins only: 
+» /pin: silently pins the message replied to - add 'loud' or 'notify' to give notifs to users
+» /unpin: unpins the currently pinned message
+» /invitelink: gets invitelink
+» /promote: promotes the user replied to
+» /lowpromote: promotes the user replied to with half rights
+» /fullpromote: promotes the user replied to with full rights
+» /demote: demotes the user replied to
+» /title <title here>: sets a custom title for an admin that the bot promoted
+» /admincache: force refresh the admins list
+» /del: deletes the message you replied to
+» /purge: deletes all messages between this and the replied to message.
+» /purge <integer X>: deletes the replied message, and X messages following it if replied to a message.
+» /setgtitle <text>: set group title
+» /setgpic: reply to an image to set as group photo
+» /setdesc: Set group description
+» /setsticker: Set group sticker
 """
 
 
